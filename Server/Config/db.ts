d4f3377301ac16be0c6099be7d4bfcea84ba076d
Adonai Ford-williams <adonai.fordwilliams@dcmail.ca>
2022-05-17 11:03:29 -0400

    Added website files

---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/contacts";
+export const RemoteURI = "mongodb+srv://Adonai:miiHqVdjdLcb8Eaz@cluster0.nnlpw.mongodb.net/contacts?retryWrites=true&w=majority"
+export const HostName = "MongoDB Atlas";
+export const SessionSecret = "WEBD6201SessionSecret";
